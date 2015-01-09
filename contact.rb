717ae060b31fc70e5573de396234a6dd7af0d63f
Mayra Cabrera <mcabrera1087@gmail.com>
2015-01-09 17:50:14 -0600

    Renames print_footer method to print_results to make it more readable

--- contact.rb
+++ contact.rb
@@ -13,26 +13,26 @@ class Contact < Base
     api_statement = generate_statement "WHERE email = '#{email}'"
     page = @service.get_contacts_by_statement api_statement.toStatement
 
-    print_footer page[:results].first, api_statement
+    print_results page[:results].first, api_statement
   end
 
   def find_contact_by_id contact_id
     api_statement = generate_statement "WHERE id = '#{contact_id}'"
     page = service.get_contacts_by_statement api_statement.toStatement
 
-    print_footer page[:results].first, api_statement
+    print_results page[:results].first, api_statement
   end
 
   def find_contacts_by_company_id company_id
     api_statement = generate_statement "WHERE companyId = #{company_id}"
     page = service.get_contacts_by_statement api_statement.toStatement
 
-    print_footer page[:results].first, api_statement
+    print_results page[:results].first, api_statement
   end
 
   private
 
-  def print_footer results, statement
+  def print_results results, statement
     if results
       puts "%d) Contact id: %d, name: '%s', company_id: '%s'" % [statement.offset + 1, results[:id], results[:name], results[:company_id]]
     end
