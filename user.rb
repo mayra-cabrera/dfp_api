4ceb1e0d6af3932993e1d1eb5f8ac4460613c234
Mayra Cabrera <mcabrera1087@gmail.com>
2015-01-19 16:19:55 -0600

    Add a find_by_role method in UserService, since there's no wrapper for roles functionality

--- user.rb
+++ user.rb
@@ -2,24 +2,32 @@ require_relative 'base.rb'
 
 class User < Base
   attr_accessor :service
+  TRAFFICKERS = 9643
 
   def initialize
     super
     @service = generate_service "UserService"
   end
 
-  def find_user_by_id user_id
+  def find_by_id user_id
     api_statement = generate_statement "WHERE id = '#{user_id}'"
     page = @service.get_users_by_statement api_statement.toStatement
 
-    print_footer page[:results].first, api_statement
+    print_results page[:results] if[:results]
+  end
+
+  def find_by_role role_id
+    api_statement = generate_statement "WHERE roleId = '#{role_id}'"
+    page = @service.get_users_by_statement api_statement.toStatement
+
+    print_results page[:results] if [:results]
   end
 
   private
 
-  def print_footer results, statement
-    if results
-      puts "%d) User id: %d, name: '%s" % [statement.offset + 1, results[:id], results[:name]]
+  def print_results results
+    results.each_with_index do |user, index|
+      puts "%d) User id: %d, name: '%s'" % [index + 1, user[:id], user[:name], user[:rolename]]
     end
   end
 end
